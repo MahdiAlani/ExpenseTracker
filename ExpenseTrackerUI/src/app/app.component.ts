cc9ff1cb7b39caddbb911e513ecd5993a9a3ba69
Mahdi Alani <themahdi67@gmail.com>
2024-07-13 21:15:56 -0400

    Added Sign in and Sign up pages

--- ExpenseTrackerUI/src/app/app.component.ts
+++ ExpenseTrackerUI/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { AddReceiptComponent } from './Receipt Entry/add-receipt/add-receipt.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { SignUpPageComponent } from "./sign-up-page/sign-up-page.component";
+import { SignUpPageComponent } from "./authPages/sign-up-page/sign-up-page.component";
+import { SignInPageComponent } from "./authPages/sign-in-page/sign-in-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AddReceiptComponent, NavBarComponent, SignUpPageComponent],
+  imports: [AddReceiptComponent, NavBarComponent, SignUpPageComponent, SignInPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
